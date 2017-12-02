39723d8ccda17a2bee5ae69b9356fc07bc74a067
Mark Hernandez <markedcode@gmail.com>
2017-12-02 15:54:37 -0600

    Add css grid of player ui

---
+++ src/App/components/PlayerUI.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+export class PlayerUI extends Component {
+  render () {
+    return (
+      <div className='player-ui'>
+        <div className='entity-list' />
+
+        <div className='text-ui' />
+
+        <div className='actions'>
+          <button>Something</button>
+        </div>
+      </div>
+    )
+  }
+}
--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import {PlayerUI} from './components'
 
 export class App extends Component {
   render () {
     return (
-      <div>
-        <h1 className='app-title'>App</h1>
+      <div id='App'>
+        <PlayerUI />
       </div>
     )
   }
